bcb372c4fe518c61ce25ac68b1648f868cf00601
LadyChristina <christina_ovezik@outlook.com>
2024-08-19 13:48:56 +0300

    Use double quotes in docstrings

--- main.py
+++ main.py
@@ -13,10 +13,10 @@ import matplotlib.pyplot as plt
 
 
 def load_dataframe(ledger):
-    '''
+    """
     :param ledger: the name of the ledger/.csv file to be imported (.csv file should have the date in rows and the metrics in columns)
     :return: cleaned up pandas dataframe with the date as the index and metrics in columns
-    '''
+    """
     df = pd.read_csv(f'input/{ledger}.csv', header=0)
     df = df.rename(columns={'Unnamed: 0': 'date'})
     df['date'] = pd.to_datetime(df['date'])
@@ -24,27 +24,27 @@ def load_dataframe(ledger):
 
 
 def set_observation_window(df, event_window, days_prior):
-    '''
+    """
     This function simply helps to return a dataframe of the observation window we specify
     :param df: dataframe in format that is outputted from load_dataframe
     :param event_window: the window that the event occurred in the format of ['yyyy-mm-dd', 'yyyy-mm-dd']
     :param days_prior: how many days before the event start should we build the observation window from
     :return: cleaned up pandas dataframe of data from only the observation window of interest
-    '''
+    """
     window = df[df["date"].between(datetime.strptime(event_window[0], '%Y-%m-%d') - timedelta(days=days_prior),
                                    event_window[0])]
     return window
 
 
 def pick_best_model(df, event_window, estimated_data, meter):
-    '''
+    """
     The purpose of this function is to compare 3 different lengths of observation window - 30 days, 60 days and 90 days- along with 3 different models - means, linear and quadratic - to return the combination with the lowest AIC
     :param df: dataframe created by loading function above
     :param event_window: the window that the event occurred in the format of ['yyyy-mm-dd', 'yyyy-mm-dd']
     :param estimated_data: the dataframe only for the dates within the event_window
     :param meter: metric of interest
     :return: AIC = optimized AIC, length of window = length of optimized observation window, esp_to_use = estimation window predictions to use, and the best_name = name of optimized model
-    '''
+    """
     model_aic = math.inf
     models = {"zero": zero_order, "linear": linear_order,
               "quadratic": quadratic_order}  # dictionary of the model names and their functions
@@ -89,11 +89,11 @@ def run_test(ledger, meter, event_window):
 
 
 def test_significance(abreturns):
-    '''
+    """
     Using a permutation test, this function determines if the abnormal returns differ significantly from zero.
     :param abreturns: difference between the observed and expected values
     :return: if the results are significant according to the permutation test
-    '''
+    """
     p = nnstats.permtest_1samp(np.array(abreturns), 0.0)[-1]
     if p > 0.05:
         return False
